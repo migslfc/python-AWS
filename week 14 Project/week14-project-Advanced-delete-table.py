a8a1d614f1e51b47760d3c1950eb579e23af7028
MIGUEL CAMACHO <124113996+migslfc@users.noreply.github.com>
2023-04-16 17:25:01 -0400

    Update week14-project-Advanced-delete-table.py

--- week 14 Project/week14-project-Advanced-delete-table.py
+++ week 14 Project/week14-project-Advanced-delete-table.py
@@ -1,12 +1,19 @@
 # week14 Project Advanced - delete table
 
-
+# Import the boto3 library
 import boto3
 
+# Initialize a DynamoDB resource
 dynamodb = boto3.resource('dynamodb')
+
+# Specify the name of the table to delete
 table_name = 'python-outside-tech'
 
+# Get the table by name
 table = dynamodb.Table(table_name)
+
+# Delete the table
 table.delete()
 
+# Print a success message
 print(f'Table {table_name} deleted successfully.')
